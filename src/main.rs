7dd1ef1fc51967c920ad8b74f65a46f879e05958
Nathan <nathan.titirangi@gmail.com>
2025-04-29 11:23:37 +1200

    Basic arg parsing setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Input compressed file
+    name: PathBuf,
+
+    /// Sets a custom config file
+    #[arg(short, long, default_value_t = 0)]
+    skip: usize,
+
+    #[arg(short, long)]
+    output: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!(
+        "name: {:?}, skip: {:?}, output: {:?}",
+        cli.name, cli.skip, cli.output
+    );
 }

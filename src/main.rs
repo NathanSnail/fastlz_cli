9ff013713c3e0d4c6dab47ef5954808137111bf1
Nathan <nathan.titirangi@gmail.com>
2025-05-14 19:04:46 +1200

    add compress mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{BufRead, BufReader, BufWriter, Read, Write},
+    io::{BufReader, BufWriter, Read, Write},
     path::PathBuf,
 };
 
@@ -14,6 +14,10 @@ struct Cli {
     /// Input compressed file
     compressed: PathBuf,
 
+    /// If this is enabled then compress instead
+    #[arg(short, long, default_value_t = false)]
+    compress: bool,
+
     /// Skip this many bytes
     #[arg(short, long, default_value_t = 0)]
     skip: usize,
@@ -26,24 +30,45 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
 
-    let mut reader =
-        BufReader::new(File::open(cli.compressed).expect("Compressed source file should exist"));
-    let mut buf = vec![0; cli.skip]; // a bit wasteful
+    let file = File::open(cli.compressed).expect("Compressed source file should exist");
+    let file_size = file.metadata().expect("File metadata must exist").len();
+    let mut reader = BufReader::new(file);
+    let mut skip_buf = vec![0; cli.skip]; // a bit wasteful
     reader
-        .read_exact(&mut buf)
+        .read_exact(&mut skip_buf)
         .expect("File should always contain at least skip bytes");
-    let compressed_size = reader.read_le::<u32>();
-    println!("{compressed_size}");
-    let decompressed_size = reader.read_le::<u32>();
-    println!("{decompressed_size}");
-    let mut compressed_buf = vec![0; compressed_size as usize];
+
+    let input_size = if cli.compress {
+        file_size as u32
+    } else {
+        reader.read_le::<u32>()
+    };
+
+    let output_size = if cli.compress {
+        std::cmp::max(66 + 8, input_size * 2)
+    } else {
+        reader.read_le::<u32>()
+    };
+
+    let mut input_buf = vec![0; input_size as usize];
+    let mut output_buf = vec![0; output_size as usize];
+
     reader
-        .read_exact(&mut compressed_buf)
-        .expect("Compressed source should always contain bytes indicated by compressed size");
-    let mut decompressed_buf = vec![0; decompressed_size as usize];
-    fastlz::decompress(&compressed_buf, &mut decompressed_buf)
-        .expect("Decompressing should never fail on compressed data");
+        .read_exact(&mut input_buf)
+        .expect("Input should always contain bytes indicated by input size");
+
+    println!("input size: {input_size}, output size: {output_size}");
+    if cli.compress {
+        let wrote_size = fastlz::compress(&input_buf, &mut output_buf[8..])
+            .expect("Decompressing should never fail on compressed data")
+            .len();
+        output_buf[..4].copy_from_slice(&(wrote_size as u32).to_le_bytes());
+        output_buf[4..8].copy_from_slice(&input_size.to_le_bytes());
+    } else {
+        fastlz::decompress(&input_buf, &mut output_buf)
+            .expect("Decompressing should never fail on compressed data");
+    }
     BufWriter::new(File::create(cli.output).expect("Should be able to create output file"))
-        .write_all(&decompressed_buf)
+        .write_all(&output_buf)
         .expect("Should be able to write decompressed content");
 }
